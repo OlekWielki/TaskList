fab84374e6b531fcc0d4c104ca6801ba4658d1c1
OlekWielki <110064330+OlekWielki@users.noreply.github.com>
2024-02-28 13:50:02 +0100

    Update index.php

--- task/index.php
+++ task/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,20 +9,30 @@
 <body>
     <header>
         <h1>Task Manager</h1>
+        <span id="login">
+            <form action="login.php">
+                <input type="submit" value="Zaloguj się" id="zaloguj" name="zaloguj"/>
+            </form>
+            <form action="register.php">
+                <input type="submit" value="Zarejestruj się" id="rejestruj" name="rejsetruj"/>
+            </form>
+        </span>
     </header>
-  <main>
-    <form method="POST">
-      <Label for="zadanie">Task</Label><br>
-      <input type="text" name="zadanie"><br>
-      <Label for="data">Data i czas</Label><br>
-      <input type="datetime-local" name="data"><br>
-      <input type="submit">
-    </form>
-    <a href="register.php">DDD</a>
-    <a href="login.php">logowanie</a>
-  </main>
-  <footer>
-      <p>Olek & Szymon Inc.</p>
-  </footer>
+    <main>
+        <form metohd="POST">
+            <span id="formularz">
+                <Label for="zadanie">Task</Label><br>
+                <input type="text" name="zadanie" required><br>
+                <Label for="data">Data i czas</Label><br>
+                <input type="datetime-local" name="data" required><br>
+            </span>
+            <span id="przycisk">
+                <input type="submit" name="submit" id="button">
+            </span>
+        </form>
+    </main>
+    <footer>
+        <p>Olek & Szymon Inc.</p>
+    </footer>
 </body>
 </html>
