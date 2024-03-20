37ec6fe98853ee9c6018d33a4d3008b6283a9502
OlekWielki <110064330+OlekWielki@users.noreply.github.com>
2024-03-20 13:42:28 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        session_start();    
-        if($_SESSION['login']==""){
-            echo "nic nie ma";
-            header('Location: register.php');
-        }
-        else{
-            echo $_SESSION['login'];
-        }
-    ?>
     <header>
         <h1>Task Manager</h1>
         <span id="login">
@@ -29,7 +19,7 @@
         </span>
     </header>
     <main>
-        <form method="POST">
+        <form method="post">
             <span id="formularz">
                 <Label for="zadanie">Task</Label><br>
                 <input type="text" name="zadanie" id="zadanie" required><br>
@@ -37,31 +27,37 @@
                 <input type="datetime-local" name="data" id="data" required><br>
             </span>
             <span id="przycisk">
-                <input type="submit" name="submit" id="button">
+                <input type="submit" name="submit" id="submit">
             </span>
         </form>
         <?php
-            $con = new Mysqli("localhost", "root" , "", "users");
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $task = $_POST["zadanie"];
-                $date = $_POST["data"];
-                echo $date;
-                $name = $_SESSION['login'];
-                $sql = "SELECT id, users.name FROM users WHERE users.name = '$name'";
-                $wyniksql = $con->query($sql);
-                $row = mysqli_fetch_row($wyniksql);
-                $sql2 = "INSERT INTO tasks VALUES('','$row[0]','$task','$date')";
-                $con->query($sql2);
-                $sql3="SELECT CAST(data as DATETIME(0)) FROM tasks WHERE id = 10";
-                $wyniksql = $con->query($sql3);
-                $row = mysqli_fetch_row($wyniksql);
-                echo $row[0];
+            session_start();
+            
+            if($_SESSION['login']==""){
+                echo "nic nie ma";
+                header('Location: register.php');
+            }
+            else{
+                echo $_SESSION['login'];
+                if(isset($_POST['submit'])){
+                    $con = new Mysqli("localhost", "root" , "", "users");
+                    $task = $_POST['zadanie'];
+                    $date = $_POST['data'];
+                    $name = $_SESSION['login'];
+                    $sql="INSERT INTO tasks VALUES('','$name','$task','$date')";
+                    $con->query($sql);
+                }
             }
         ?>
     </main>
     <section>
         <?php
-            
+            $con = new Mysqli("localhost", "root" , "", "users");
+            $sql="SELECT task, date FROM tasks WHERE name='$name'";
+            $wiersz = $con->query($sql);
+            while($wynik = $wiersz->fetch_row()){
+                echo "$wynik[0] $wynik[1] <br>";
+            }
         ?>
     </section>
     <footer>
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-<form action="login.php" method="post">
+<form method="post">
     Username: <input type="text" name="username"><br>
     Password: <input type="password" name="password"><br>
     <input type="submit" value="Login">
