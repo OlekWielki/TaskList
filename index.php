b148b623a13fdfbb4f02c9075eba172c95ce031a
Szymonek225 <165900889+Szymonek225@users.noreply.github.com>
2024-04-03 13:51:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -42,25 +42,52 @@
                 unset($_POST['submit']);
                 header("Location: index.php");
             }
+            
         }
     ?>
     </main>
     <section id="taski">
-        <?php
-            
-            $name = $_SESSION['login'];
-            $con = new Mysqli("localhost", "root" , "", "users");
-            $sql="SELECT task, date FROM tasks WHERE name='$name' ORDER BY date ASC";
-            $wiersz = $con->query($sql);
-            while($wynik = $wiersz->fetch_row()){
-                echo "<form method='POST' class='wyswietl' id='formcont'>";
-                echo "<div class='taskcontainer'>$wynik[0] $wynik[1]";
-                echo "<input type='submit' id='usun' name='usun' value='usun'>";
-                echo "</div><br>";
-                echo "</form>";
-            }
-            
-        ?>
+    <?php
+    $name = $_SESSION['login'];
+    $con = new Mysqli("localhost", "root", "", "users");
+    $sql = "SELECT id, task, date FROM tasks WHERE name='$name' ORDER BY date ASC";
+    $wiersz = $con->query($sql);
+    while ($wynik = $wiersz->fetch_assoc()) {
+        echo "<form method='POST' class='wyswietl'>";
+        echo "<div class='taskcontainer' id='task_$wynik[id]'>$wynik[task] $wynik[date]";
+        echo "<input type='hidden' name='task_id' value='$wynik[id]'>";
+        echo "<input type='submit' class='usun' name='usun' value='usun'>";
+        echo "</div><br>";
+        echo "<input type='text' name='change'>";
+        echo "<input type='submit' class='zmien' name='zmien' value='zmien'>";
+        echo "<br>";
+        echo "</form>";
+}
+
+if (isset($_POST['usun']) && isset($_POST['task_id'])) {
+    $task_id = $_POST['task_id'];
+    $sql = "DELETE FROM tasks WHERE id='$task_id'";
+    if ($con->query($sql) === TRUE) {
+
+    } else {
+        echo "<script>alert('Błąd podczas usuwania rekordu z bazy danych: " . $con->error . "');</script>";
+    }
+    header("Location: index.php");
+}
+if (isset($_POST['zmien']) && isset($_POST['task_id']) && isset($_POST['change'])) {
+        $task_id = $_POST['task_id'];
+        $new_date = $_POST['change'];
+    
+        
+        $sql = "UPDATE tasks SET date='$new_date' WHERE id='$task_id'";
+        if ($con->query($sql) === TRUE) {
+            header("Location: index.php");
+            exit; 
+        } else {
+            echo "<script>alert('Błąd podczas aktualizowania daty w bazie danych: " . $con->error . "');</script>";
+        }
+    }
+?>
     </section>
     <footer>
         <p>Olek & Szymon Inc.</p>
