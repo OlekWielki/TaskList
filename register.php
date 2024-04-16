b2b4f3aca945245f70827abb62be5bd9dcdbe1ab
Szymonek225 <165900889+Szymonek225@users.noreply.github.com>
2024-04-16 10:20:02 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="styleregister.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+
+<div id="tlo">
+<div id="login">
     <form method = "POST">
+
+        <h1>REJESTRACJA</h1><br>
+        <Label for="username">Username</Label>
         <input type="text" name = "username">
+        <Label for = "password">password</Label>
         <input type="password" name = "password">
         <input type="submit">
     </form>
+</div></div>
     
     <p>Masz już konto? <a href = "login.php">Zaloguj<a></p>
     <?php 
