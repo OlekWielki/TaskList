8a9a48cb2e6a0575dbd6ec75525bb6c79aa21c12
Szymonek225 <165900889+Szymonek225@users.noreply.github.com>
2024-05-31 16:07:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
         <h1>Task Manager</h1>
         <span id="login">
             <a href="profile.php"><img src="profile.png" alt="profilowe"></a>
+            <form method="Post">
+                <input type="submit" value = "wyloguj się" name = "wyloguj">
+            </form>
+            <?php
+                if(isset($_POST['wyloguj'])){
+                    $_SESSION['login'] = "";
+                    header('Location: register.php');
+                }
+            ?>
         </span>
     </header>
     <main>
@@ -27,76 +36,91 @@
         </form>
         <?php 
           session_start();
-          if($_SESSION['login']==""){
-            echo "nic nie ma";
-            header('Location: register.php');
-        }
-        else{
-            if(isset($_POST['submit'])){
-                $con = new Mysqli("localhost", "root" , "", "users");
-                $task = $_POST['zadanie'];
-                $date = $_POST['data'];
-                $name = $_SESSION['login'];
-                
-        
-                $currentDateTime = date('Y-m-d H:i:s');
-                if($date < $currentDateTime) {
-                    echo "<script>alert('Nie można dodać zadania z datą wcześniejszą niż dzisiejsza.');</script>";
-                } else {
-                    $sql="INSERT INTO tasks VALUES('','$name','$task','$date')";
-                    $con->query($sql);
-                    unset($_POST['submit']);
-                    header("Location: index.php");
-                }
-            }
-            
-        }
-    ?>
-    </main>
-    <section id="taski">
-    <?php
-        $name = $_SESSION['login'];
-        $con = new Mysqli("localhost", "root", "", "users");
-        $sql = "SELECT id, task, date FROM tasks WHERE name='$name' ORDER BY date ASC";
-        $wiersz = $con->query($sql);
-        while ($wynik = $wiersz->fetch_assoc()) {
-            echo "<form method='POST' class='wyswietl'>";
-            echo "<div class='taskcontainer' id='task_$wynik[id]'>$wynik[task] $wynik[date]";
-            echo "<input type='hidden' name='task_id' value='$wynik[id]'>";
-            echo "<input type='submit' class='usun' name='usun' value='usun'>";
-            echo "<br>";
-            echo "<input type='datetime-local' name='change'>";
-            echo "<input type='submit' class='zmien' name='zmien' value='zmien'>";
-            echo "</div><br>";
-            echo "</form>";
-    }
+          if ($_SESSION['login'] == "") {
+              echo "nic nie ma";
+              header('Location: register.php');
+          } else {
+              if (isset($_POST['submit'])) {
+                  $con = new mysqli("localhost", "root", "", "users");
+                  $task = $_POST['zadanie'];
+                  $date = $_POST['data'];
+                  $name = $_SESSION['login'];
+                  
+                  $currentDateTime = date('Y-m-d H:i:s');
+                  if ($date < $currentDateTime) {
+                      echo "<script>alert('Nie można dodać zadania z datą wcześniejszą niż dzisiejsza.');</script>";
+                  } else {
 
-    if (isset($_POST['usun']) && isset($_POST['task_id'])) {
-        $task_id = $_POST['task_id'];
-        $sql = "DELETE FROM tasks WHERE id='$task_id'";
-        $con->query($sql);
-        header("Location: index.php");
-    }
-    if (isset($_POST['zmien']) && isset($_POST['task_id']) && isset($_POST['change'])) {
+                      $formattedDate = date('Y-m-d H:i:s', strtotime($date));
+                      $sql = "INSERT INTO tasks (name, task, date) VALUES ('$name', '$task', '$formattedDate')";
+                      $sql2 = "UPDATE profile SET task_count = task_count + 1 where task_name = '$name'";
+                      $con->query($sql);
+                      $con->query($sql2);
+                      unset($_POST['submit']);
+                      header("Location: index.php");
+                  }
+              }
+          }
+          ?>
+          
+          <section id="taski">
+          <?php
+          $name = $_SESSION['login'];
+          $con = new mysqli("localhost", "root", "", "users");
+          $sql = "SELECT id, task, DATE_FORMAT(date, '%Y-%m-%d %H:%i:%s') as date FROM tasks WHERE name='$name' ORDER BY date ASC";
+          $wiersz = $con->query($sql);
+          while ($wynik = $wiersz->fetch_assoc()) {
+              echo "<form method='POST' class='wyswietl'>";
+              echo "<div class='taskcontainer' id='task_$wynik[id]'>$wynik[task] $wynik[date]";
+              echo "<input type='hidden' name='task_id' value='$wynik[id]'>";
+              echo "<input type='submit' class='usun' name='usun' value='usun'>";
+              echo "<br>";
+              echo "<input type='datetime-local' name='change'>";
+              echo "<input type='submit' class='zmien' name='zmien' value='zmien'>";
+              echo "<input type='submit' class='zrobione' name='zrobione' value='zrobione'>";
+              echo "</div><br>";
+              echo "</form>";
+          }
+          
+          if (isset($_POST['usun']) && isset($_POST['task_id'])) {
             $task_id = $_POST['task_id'];
-            $new_date = $_POST['change'];
-        
-            
-            $sql = "UPDATE tasks SET date='$new_date' WHERE id='$task_id'";
-            if ($con->query($sql) === TRUE) {
-                header("Location: index.php");
-                exit; 
-            } else {
-                echo "<script>alert('Błąd podczas aktualizowania daty w bazie danych: " . $con->error . "');</script>";
-            }
-        }
-    ?>
+            $name = $_SESSION['login'];
+            $con->query($sql);
+            $sql1 = "DELETE FROM tasks WHERE id='$task_id'";
+            $con->query($sql1);
+            header("Location: index.php");
+          }
+          if (isset($_POST['zrobione']) && isset($_POST['task_id'])) {
+            $task_id = $_POST['task_id'];
+            $name = $_SESSION['login'];
+
+            $sql = "UPDATE profile SET task_done = task_done + 1 where task_name = '$name'";
+            $con->query($sql);
+            $sql1 = "DELETE FROM tasks WHERE id='$task_id'";
+            $con->query($sql1);
+            header("Location: index.php");
+          }
+          if (isset($_POST['zmien']) && isset($_POST['task_id']) && isset($_POST['change'])) {
+              $task_id = $_POST['task_id'];
+              $new_date = $_POST['change'];
+              $formattedNewDate = date('Y-m-d H:i:s', strtotime($new_date));
+              
+              $sql = "UPDATE tasks SET date='$formattedNewDate' WHERE id='$task_id'";
+              if ($con->query($sql) === TRUE) {
+                  header("Location: index.php");
+                  exit; 
+              } else {
+                  echo "<script>alert('Błąd podczas aktualizowania daty w bazie danych: " . $con->error . "');</script>";
+              }
+          }
+          ?>
     </section>
     <footer>
         <p>Olek & Szymon Inc.</p>
     </footer>
     <script>
 
+var zrobione = false;
 function wyswietlAlert() {
     <?php
         $sql="SELECT task, date FROM tasks WHERE name='$name' ORDER BY date ASC";
@@ -123,21 +147,16 @@ function wyswietlAlert() {
         dzisiaj.getFullYear() + '-' + dodajZero(miesiac) + '-' + dodajZero(dzien) + ' ' +
         dodajZero(godzina) + ':' + dodajZero(minuta) + ":00.000000";
         
-    var alertDisplayed = localStorage.getItem('alertDisplayed');
-    if (!alertDisplayed && dateandtime === godzinaZPHP) {
+    if (zrobione == false && dateandtime === godzinaZPHP) {
         alert("wykonaj zadanie " + taskZPHP);
-        
-        localStorage.setItem('alertDisplayed', true);
-    }
-    else{
-        localStorage.setItem('alertDisplayed', false);
 
+        zrobione = true;
     }
-
     console.log(dateandtime);
     console.log(godzinaZPHP);
 }
 setInterval(wyswietlAlert, 1000);
+
 </script>
 </body>
 </html>
